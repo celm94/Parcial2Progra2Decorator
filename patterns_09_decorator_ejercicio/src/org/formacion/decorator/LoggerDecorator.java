c36317db962aba85ee6d6d32f1c9bee2b5927694
Carlos Hernandez <chernandezl19@miumg.edu.gt>
2021-09-13 17:58:08 -0600

    initial

---
+++ patterns_09_decorator_ejercicio/src/org/formacion/decorator/LoggerDecorator.java
@@ -0,0 +1,20 @@
+package org.formacion.decorator;
+
+import java.util.List;
+
+public class LoggerDecorator implements BaseDatos {
+	
+	
+
+	@Override
+	public void inserta(String registro) {
+	}
+
+	@Override
+	public List<String> registros() {
+		return null;
+	}
+
+	
+
+}
