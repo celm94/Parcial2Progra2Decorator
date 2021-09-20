972cf5b2088341d7398fb045fde1b3ddbd2ddd70
Célita Morales <cmoralesm37@miumg.edu.gt>
2021-09-20 12:29:22 -0600

    test listo

--- patterns_09_decorator_ejercicio/src/org/formacion/decorator/LoggerDecorator.java
+++ patterns_09_decorator_ejercicio/src/org/formacion/decorator/LoggerDecorator.java
@@ -3,16 +3,28 @@ package org.formacion.decorator;
 import java.util.List;
 
 public class LoggerDecorator implements BaseDatos {
+
+	private final BaseDatos baseDatos;
+	private final Logger logger;
+
+	public LoggerDecorator(BaseDatos baseDatos, Logger logger){
+		this.baseDatos = baseDatos;
+		this.logger = logger;
+	}
 	
 	
 
 	@Override
 	public void inserta(String registro) {
+		logger.add("inserta" + registro);
+		baseDatos.inserta(registro);
 	}
 
 	@Override
 	public List<String> registros() {
-		return null;
+
+		logger.add("lectura");
+		return baseDatos.registros();
 	}
 
 	
